8df75b8baa32ba81963cf5644204f98a5fca295d
MAshraf <ee.mashraf@gmail.com>
2015-12-14 18:42:57 +0200

    avoid null

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,8 @@
 class Device < ActiveRecord::Base
   belongs_to :room
+  validates :light, presence: true
+  validates :ac, presence: true
+  validates :shade, presence: true
+  validates :door, presence: true
+  validates :camera, presence: true
 end
